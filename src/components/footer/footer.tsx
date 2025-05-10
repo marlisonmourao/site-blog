b580b2b6e544e2415801f13283d72d3a0b02ee89
marlisonmourao <bentesmourao@gmail.com>
2025-05-10 19:34:58 -0400

    Update Tailwind CSS configuration and component styles; enhance layout with new font integration and adjust footer and button styles for improved UI consistency.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,22 +3,22 @@ import { Logo } from '../logo'
 
 export function Footer() {
   return (
-    <footer className="border-t border-white/10">
+    <footer className="bg-gray-500">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-5">
         <div className="flex justify-between md:flex-row gap-8 py-8">
           <Logo />
 
-          <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
-            <Link href="/" className="hover:text-primary">
+          <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-blue-100">
+            <Link href="/" className="hover:text-blue-200">
               Termos de uso
             </Link>
             <Link
               href="/politica-de-privacidade"
-              className="hover:text-primary"
+              className="hover:text-blue-200y"
             >
               Política de privacidade
             </Link>
-            <Link href="/feedback" className="hover:text-primary">
+            <Link href="/feedback" className="hover:text-blue-200">
               Enviar feedback
             </Link>
           </nav>
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,9 +1,16 @@
+import { Inter } from 'next/font/google'
 import { Footer } from '../footer/footer'
 import { Header } from '../header'
 
+const inter = Inter({
+  subsets: ['latin'],
+})
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex  min-h-screen flex-col dark">
+    <div
+      className={`${inter.className} relative bg-gray-700 flex min-h-screen flex-col dark`}
+    >
       <Header />
 
       <main className="flex-1 flex flex-col mb-12">{children}</main>
