204e6ea82a00663fcb99122540d3bcfcc083c78d
marlisonmourao <bentesmourao@gmail.com>
2025-05-14 19:02:39 -0400

    Add PostGridCard component to enhance blog list layout; update BlogList to wrap PostCard in PostGridCard for improved grid presentation.

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,6 +1,7 @@
 import { Search } from '@/components/search'
 import { useRouter } from 'next/router'
 import { PostCard } from './components/post-card'
+import { PostGridCard } from './components/post-grid-card'
 
 export function BlogList() {
   const router = useRouter()
@@ -12,7 +13,7 @@ export function BlogList() {
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
-      <header className="">
+      <header className="pb-14">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
@@ -27,15 +28,17 @@ export function BlogList() {
         </div>
       </header>
 
-      <PostCard
-        slug="transformando-seu-negocio-com-o-site-set"
-        title="Transformando seu negócio com o site.set"
-        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site ...
+      <PostGridCard>
+        <PostCard
+          slug="transformando-seu-negocio-com-o-site-set"
+          title="Transformando seu negócio com o site.set"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site ...
         "
-        image="/assets/post.png"
-        date="20/12/2024"
-        author={{ name: 'Marlison Mourão', avatar: '/assets/avatar.png' }}
-      />
+          image="/assets/post.png"
+          date="20/12/2024"
+          author={{ name: 'Marlison Mourão', avatar: '/assets/avatar.png' }}
+        />
+      </PostGridCard>
     </div>
   )
 }
