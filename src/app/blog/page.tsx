0485e472896df67ea810413030e8eb057de64d6d
marlisonmourao <bentesmourao@gmail.com>
2025-05-18 20:22:57 -0400

    refactor: migrate pages to app directory and improve blog structure

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,12 @@
 import { BlogList } from '@/templates/blog/blog-list'
 import { allPosts } from 'contentlayer/generated'
 
+export async function generateStaticParams() {
+  return allPosts.map(post => ({
+    slug: post.slug,
+  }))
+}
+
 export default async function BlogPage() {
   const sortedPosts = await allPosts.sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,38 +2,32 @@
 
 import { Button } from '@/components/ui/button'
 import { AlertTriangle } from 'lucide-react'
-import { useEffect } from 'react'
 
-type ErrorComponentProps = {
+type ErrorBoundaryProps = {
   error: Error
   reset: () => void
 }
 
-export default function ErrorBoundary({ error, reset }: ErrorComponentProps) {
-  useEffect(() => {
-    throw new Error('Error in ErrorComponent')
-  }, [])
-
+export default function ErrorBoundary({ error, reset }: ErrorBoundaryProps) {
   return (
     <div className="min-h-[400px] flex items-center justify-center p-4">
-      <div className="w-full max-w-md text-center">
+      <div className="max-w-md w-full text-center">
         <div className="relative inline-block mb-6">
-          <AlertTriangle className="text-gray-100 mx-auto" size={64} />
-
-          <div className="absolute h-1 w-16 bg-red-500 -rotate-12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-80" />
-
-          <h2 className="text-heading-xl text-white font-sans mb-2">
-            Something went wrong
-          </h2>
-
-          <p className="text-gray-200 mb-2 text-body-sm overflow-hidden text-ellipsis max-h-24">
+          <AlertTriangle size={64} className="text-gray-100 mx-auto" />
+          <div className="absolute h-1 w-16 bg-red-500 -rotate-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-80" />
+        </div>
+        <h2 className="text-heading-xl text-white font-sans mb-2">
+          Something went wrong!
+        </h2>
+        {error?.message && (
+          <p className="text-gray-200 mb-2 px-4 text-body-md overflow-hidden text-ellipsis max-h-24">
             {error?.message}
           </p>
+        )}
 
-          <Button variant="primary" onClick={reset} className="mt-4">
-            Try again
-          </Button>
-        </div>
+        <Button variant="primary" onClick={reset} className="mt-4">
+          Try again
+        </Button>
       </div>
     </div>
   )
