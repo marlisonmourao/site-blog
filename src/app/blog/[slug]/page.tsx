317e5a7b7a27f62cc4b4f4ef25e583f3694ed068
marlisonmourao <bentesmourao@gmail.com>
2025-05-23 21:12:47 -0400

    feat: enhance metadata generation for blog posts and layout with Open Graph support, including detailed descriptions and images

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,33 @@
 import { PostPage } from '@/templates/blog/post-page'
 import { allPosts } from 'contentlayer/generated'
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params
+
+  const post = allPosts.find(post => post.slug === slug)
+
+  if (!post) {
+    return {
+      title: 'Post não encontrado',
+      description: 'Post não encontrado',
+    }
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      images: [post.image],
+    },
+  }
+}
 
 type BlogPostPageProps = {
   params: Promise<{ slug: string }>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import '../styles/globals.css'
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Venda seu produtos de afiliado em um único lugar',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seu produtos de afiliado em um único lugar',
+    url: 'https://site-blog-weld.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-blog-weld.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
