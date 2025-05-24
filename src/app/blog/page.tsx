a6955e42bc3ab3e058424dbd9f477fb1407795e2
marlisonmourao <bentesmourao@gmail.com>
2025-05-23 21:08:15 -0400

    feat: update metadata for layout, home, and blog pages with Open Graph support and improved descriptions

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,27 @@
 import { BlogList } from '@/templates/blog/blog-list'
 import { allPosts } from 'contentlayer/generated'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para o seu negócio',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seu produtos de afiliado em um único lugar',
+    url: 'https://site-blog-weld.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-blog-weld.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export async function generateStaticParams() {
   return allPosts.map(post => ({
